53672ab3f98e23f67b8f64c1cbc226ef756f4a91
Bartosz Gryciuk <f3ry39@gmail.com>
2022-11-03 18:23:16 +0100

    lab4 - zadanie 1

--- Unity3D/Assets/Scripts/lab4/Zadanie4_1.cs
+++ Unity3D/Assets/Scripts/lab4/Zadanie4_1.cs
@@ -1,32 +1,41 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class Zadanie1 : MonoBehaviour
+public class Zadanie4_1 : MonoBehaviour
 {
     List<Vector3> positions = new List<Vector3>();
     public float delay = 3.0f;
+    public int iloscKostek = 1;
     int objectCounter = 0;
-    // obiekt do generowania
+
+    public Material kolor1;
+    public Material kolor2;
+    public Material kolor3;
+    public Material kolor4;
+    public Material kolor5;
     public GameObject block;
 
     void Start()
     {
-        // w momecie uruchomienia generuje 10 kostek w losowych miejscach
-        List<int> pozycje_x = new List<int>(Enumerable.Range(0, 20).OrderBy(x => Guid.NewGuid()).Take(10));
-        List<int> pozycje_z = new List<int>(Enumerable.Range(0, 20).OrderBy(x => Guid.NewGuid()).Take(10));
 
-        for (int i = 0; i < 10; i++)
+        Vector3 Max = GetComponent<MeshRenderer>().bounds.max;
+        Vector3 Min = GetComponent<MeshRenderer>().bounds.min;
+
+        List<int> pozycje_x = new List<int>(Enumerable.Range(Convert.ToInt32(Min.x), Convert.ToInt32(Max.x)).OrderBy(x => Guid.NewGuid()).Take(10));
+        List<int> pozycje_z = new List<int>(Enumerable.Range(Convert.ToInt32(Min.z), Convert.ToInt32(Max.z)).OrderBy(x => Guid.NewGuid()).Take(10));
+
+        for (int i = 0; i < iloscKostek; i++)
         {
-            this.positions.Add(new Vector3(pozycje_x[i], 5, pozycje_z[i]));
+            this.positions.Add(new Vector3(pozycje_x[i], Max.y + 5, pozycje_z[i]));
         }
         foreach (Vector3 elem in positions)
         {
             Debug.Log(elem);
         }
-        // uruchamiamy coroutine
+
         StartCoroutine(GenerujObiekt());
     }
 
@@ -37,13 +46,15 @@ public class Zadanie1 : MonoBehaviour
 
     IEnumerator GenerujObiekt()
     {
-        Debug.Log("wywo�ano coroutine");
+        List<Material> kolory = new List<Material>() { kolor1, kolor2, kolor3, kolor4, kolor5 };
+        Debug.Log("wywo³ano coroutine");
         foreach (Vector3 pos in positions)
         {
+            block.GetComponent<MeshRenderer>().material = kolory[UnityEngine.Random.Range(0, 5)];
             Instantiate(this.block, this.positions.ElementAt(this.objectCounter++), Quaternion.identity);
             yield return new WaitForSeconds(this.delay);
         }
-        // zatrzymujemy coroutine
+
         StopCoroutine(GenerujObiekt());
     }
 }
